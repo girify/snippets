d74e7f25a988d27c4b559f06dcf0508a92aff9bb
Wyatt Allen <wyatt.allen0@gmail.com>
2011-09-27 16:20:25 -0700

    Added CSharp snippets

---
+++ csharp/asp/BaseController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+namespace My.Web.Application.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public ActionResult Http404 {
+            get {
+                Response.StatusCode = 404;
+                return View("Http404");
+            }
+        }
+
+        public RedirectResult RedirectToActionWithHash(string action, object routeValues, string hash)
+        {
+            return Redirect(
+                Url.Action(action, routeValues) + "#" + hash
+            );
+        }
+
+        public RedirectResult RedirectToActionWithHash(string action, string hash)
+        {
+            return Redirect(
+                Url.Action(action) + "#" + hash
+            );
+        }
+
+        protected byte[] ReadPostedFile(string key, out string mime)
+        {
+            var file = Request.Files[key];
+
+            if (file == null)
+            {
+                mime = null;
+                return null;
+            }
+            else
+            {
+                mime = file.ContentType;
+
+                var buffer = new byte[file.ContentLength];
+                file.InputStream.Read(buffer, 0, file.ContentLength);
+                return buffer;
+            }
+        }
+    }
+}
---
+++ csharp/enumerable/IEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿﻿namespace System.Collections.Generic {
+    public static class IEnumerableExtensions {
+        public static IEnumerable<T> Cons<T>(this IEnumerable<T> self, T init) {
+            yield return init;
+            foreach (var elem in self) {
+                yield return elem;
+            }
+        }
+
+        public static string Fuse(this IEnumerable<string> self, string separator = "") {
+            return string.Join(separator, self);
+        }
+    }
+}
