472f907ac552630fb346aa7b5049790cbf3b2a8c
Wyatt Allen <wyatt.allen0@gmail.com>
2011-09-29 17:15:36 -0700

    Added simple haskell snippets

---
+++ haskell/math/primality.hs
@@ -0,0 +1,12 @@
+﻿-- From: http://stackoverflow.com/q/4541415
+-- Modified so that 1 is not prime
+isPrime 1 = False
+isPrime x = null $ filter divisible $ takeWhile notTooBig [2..] 
+    where
+        divisible y = x `mod` y == 0
+        notTooBig y = y * y <= x
+
+-- Naieve prime generator        
+primes = [ n | n <- [ 1.. ], isPrime n ]
+
+
